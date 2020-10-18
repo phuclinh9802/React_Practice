45e9ff13b8bf46f09bf3ce6c5c4d63dfc97b672e
Phillip <anicedream2002@gmail.com>
2020-10-18 17:28:16 -0500

    React tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ElecArr = [{
+  id:1,
+  title:
+    "SAMSUNG 65-inch Class QLED Q80T Series - 4K UHD Direct Full Array 12X Quantum HDR 12X Smart TV with Alexa Built-in (QN65Q80TAFXZA, 2020 Model)",
+  creator: "SAMSUNG",
+  img: "https://m.media-amazon.com/images/I/41wRWxMSX6L._AC_SY200_.jpg"
+},
+{
+  id:2,
+  title:
+    'Samsung QN65Q60TA 65" Ultra High Definition 4K Quantum HDR Smart QLED TV with a Klipsch CINEMA-600 3.1 Dynamic Power Soundbar with 10" Wireless Subwoofer (2020)',
+  creator: "SAMSUNG",
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81dJD40DFjL._AC_SL1500_.jpg"
+}];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function ElecList() {
+  return (
+    <div className="eleclist">
+        {ElecArr.map((elec)=> {
+          return <Elec key={elec.id} {...elec}/>
+        })
+        }
+    </div>
+  );
+}
+
+function Elec(props) {
+  const {img, title, creator} = props;
+  return (
+    <div className="elec">
+      <img src={img} alt="" />
+      <h2>{title}</h2>
+      <h3>{creator}</h3>
+    </div>
+  );
+}
+
+ReactDom.render(<ElecList />, document.getElementById("root"));
