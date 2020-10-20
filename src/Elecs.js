cea53d7d0aa3840d8729ef52aa2d68252bab3736
Phillip <anicedream2002@gmail.com>
2020-10-19 22:53:42 -0500

    Import/Export functions

---
+++ src/Elecs.js
@@ -0,0 +1,15 @@
+export const ElecArr = [{
+  id:1,
+  title:
+    "SAMSUNG 65-inch Class QLED Q80T Series - 4K UHD Direct Full Array 12X Quantum HDR 12X Smart TV with Alexa Built-in (QN65Q80TAFXZA, 2020 Model)",
+  creator: "SAMSUNG",
+  img: "https://m.media-amazon.com/images/I/41wRWxMSX6L._AC_SY200_.jpg"
+},
+{
+  id:2,
+  title:
+    'Samsung QN65Q60TA 65" Ultra High Definition 4K Quantum HDR Smart QLED TV with a Klipsch CINEMA-600 3.1 Dynamic Power Soundbar with 10" Wireless Subwoofer (2020)',
+  creator: "SAMSUNG-2",
+  img:
+    "https://m.media-amazon.com/images/I/51mk0P1LXqL._AC_SY161_.jpg"
+}];
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+import { ElecArr } from "./Elecs";
+import { Elec } from "./Elec";
 
-const ElecArr = [{
-  id:1,
-  title:
-    "SAMSUNG 65-inch Class QLED Q80T Series - 4K UHD Direct Full Array 12X Quantum HDR 12X Smart TV with Alexa Built-in (QN65Q80TAFXZA, 2020 Model)",
-  creator: "SAMSUNG",
-  img: "https://m.media-amazon.com/images/I/41wRWxMSX6L._AC_SY200_.jpg"
-},
-{
-  id:2,
-  title:
-    'Samsung QN65Q60TA 65" Ultra High Definition 4K Quantum HDR Smart QLED TV with a Klipsch CINEMA-600 3.1 Dynamic Power Soundbar with 10" Wireless Subwoofer (2020)',
-  creator: "SAMSUNG-2",
-  img:
-    "https://m.media-amazon.com/images/I/51mk0P1LXqL._AC_SY161_.jpg"
-}];
 
 function ElecList() {
   return (
@@ -30,25 +17,6 @@ function ElecList() {
 }
 
 
-function Elec(props) {
-  const {img, title, creator} = props;
-  const Buy = () => {
-  alert("You have bought this item!")
-  }
-  const More = (title) => {
-    alert(title);
-  }
-  return (
-    <div className="elec" onMouseOver={() => {
-      console.log(creator)
-    }}>
-      <img src={img} alt="" />
-      <h2>{title}</h2>
-      <h3>{creator}</h3>
-      <button onClick={Buy}>Buy</button>
-      <button onClick={() => More(title)}>More</button>
-    </div>
-  );
-}
+
 
 ReactDom.render(<ElecList />, document.getElementById("root"));
